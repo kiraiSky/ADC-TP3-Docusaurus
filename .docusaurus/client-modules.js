34d1759f5871290fa325c05e8f5e52c2f7d4ff72
Hanna Paiva <hannapaiva.dev@gmail.com>
2024-12-16 15:18:41 +0000

    mudança nas cores

--- .docusaurus/client-modules.js
+++ .docusaurus/client-modules.js
@@ -1,13 +1,6 @@
 export default [
-<<<<<<< HEAD
-  require('C:\\Users\\admin\\OneDrive - Universidade do Algarve\\Ambiente de Trabalho\\adc\\ADC-TP3-Docusaurus\\node_modules\\infima\\dist\\css\\default\\default.css'),
-  require('C:\\Users\\admin\\OneDrive - Universidade do Algarve\\Ambiente de Trabalho\\adc\\ADC-TP3-Docusaurus\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
-  require('C:\\Users\\admin\\OneDrive - Universidade do Algarve\\Ambiente de Trabalho\\adc\\ADC-TP3-Docusaurus\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
-  require('C:\\Users\\admin\\OneDrive - Universidade do Algarve\\Ambiente de Trabalho\\adc\\ADC-TP3-Docusaurus\\src\\css\\custom.css'),
-=======
-  require('C:\\Users\\admin\\Desktop\\ADC-TP3-Docusaurus\\node_modules\\infima\\dist\\css\\default\\default.css'),
-  require('C:\\Users\\admin\\Desktop\\ADC-TP3-Docusaurus\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
-  require('C:\\Users\\admin\\Desktop\\ADC-TP3-Docusaurus\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
-  require('C:\\Users\\admin\\Desktop\\ADC-TP3-Docusaurus\\src\\css\\custom.css'),
->>>>>>> d71289999727c134a17f83c85b0c9e9e060f7363
+  require('C:\\Users\\admin\\Desktop\\clone\\ADC-TP3-Docusaurus\\node_modules\\infima\\dist\\css\\default\\default.css'),
+  require('C:\\Users\\admin\\Desktop\\clone\\ADC-TP3-Docusaurus\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
+  require('C:\\Users\\admin\\Desktop\\clone\\ADC-TP3-Docusaurus\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
+  require('C:\\Users\\admin\\Desktop\\clone\\ADC-TP3-Docusaurus\\src\\css\\custom.css'),
 ];
