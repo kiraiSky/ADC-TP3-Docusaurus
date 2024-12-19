2d725e31761cf387bce379b35d4aaf22f3a4d5d2
João Cunha <84739839+kiraiSky@users.noreply.github.com>
2024-12-19 09:33:06 +0000

    Delete .docusaurus directory

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('C:\\Users\\admin\\Desktop\\clone\\ADC-TP3-Docusaurus\\node_modules\\infima\\dist\\css\\default\\default.css'),
-  require('C:\\Users\\admin\\Desktop\\clone\\ADC-TP3-Docusaurus\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
-  require('C:\\Users\\admin\\Desktop\\clone\\ADC-TP3-Docusaurus\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
-  require('C:\\Users\\admin\\Desktop\\clone\\ADC-TP3-Docusaurus\\src\\css\\custom.css'),
-];
